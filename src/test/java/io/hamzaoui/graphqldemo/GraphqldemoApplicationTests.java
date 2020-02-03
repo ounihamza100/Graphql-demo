fd7458a302224191bfb7cbd872e9c76f9d007eb0
ounihamza100 <hamza.ellouni@gmail.com>
2020-02-03 10:08:28 +0100

    add junit 5 test

--- src/test/java/io/hamzaoui/graphqldemo/GraphqldemoApplicationTests.java
+++ src/test/java/io/hamzaoui/graphqldemo/GraphqldemoApplicationTests.java
@@ -1,13 +1,39 @@
 package io.hamzaoui.graphqldemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GraphqldemoApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void listFoods() throws Exception {
+		String expectedResponse = "{\"data\":{\"foods\":[" +
+				"{\"id\":1,\"name\":\"Pizza\",\"isGood\":true}," +
+				"{\"id\":2,\"name\":\"Spam\",\"isGood\":false}," +
+				"{\"id\":3,\"name\":\"Eggs\",\"isGood\":true}," +
+				"{\"id\":4,\"name\":\"Avocado\",\"isGood\":false}" +
+				"]}}";
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/graphql")
+				.content("{\"query\":\"{ foods { id name isGood } }\"}")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().json(expectedResponse))
+				.andReturn();
 	}
 
 }
